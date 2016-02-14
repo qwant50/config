99673998471685da983910a57baf57566885da0f
Qwant50 <qwantonline@gmail.com>
2016-02-14 17:44:33 +0200

    chore index.php for example

--- index.php
+++ index.php
@@ -7,9 +7,7 @@ $loader = require __DIR__ . '/vendor/autoload.php';
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 1);
 
-
+//  Reads all files .php | .ini | .yaml from folder
 $config = new Config(__DIR__ . '/src/configs');
 
-//$configObj->loadConfig('component.php');
-//$configObj->loadConfigs();
 var_dump($config->getData());
